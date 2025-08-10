96773591a9210b91ba517a6d94c3c9f5b0047a42
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2025-08-10 20:28:50 +0300

    setup boilerplate with react

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* Will be used to export the project to static files */
+  // output: 'export',
+  // reactStrictMode: true,
+  // images: {
+  //   unoptimized: true,
+  // },
+};
+
+export default nextConfig;
