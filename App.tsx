ef90a4fba93456675d664798da6bf6a90ee716d7
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2026-02-03 23:32:27 +0200

    implement location hook and very basic rendering

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useLocation } from './src/hooks/useLocation';
 
 export default function App() {
+  const { location, errorMsg, loading } = useLocation();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      <Text style={styles.text}>Current coordinates:</Text>
+
+      {loading ? (
+        <Text style={styles.text}>Loading GPS...</Text>
+      ) : errorMsg ? (
+        <Text style={styles.errorText}>{errorMsg}</Text>
+      ) : (
+        <Text style={styles.coords}>
+          {location?.coords.latitude.toFixed(4)}, {location?.coords.longitude.toFixed(4)}
+        </Text>
+      )}
     </View>
   );
 }
@@ -13,8 +27,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2e2e2e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#e2e2e2',
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  coords: {
+    color: '#00e5ff',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  errorText: {
+    color: '#ff5252',
+  }
 });
