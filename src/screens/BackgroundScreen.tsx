b7ac5fee94e1f63692b10b867109b439e398fa49
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2026-02-19 23:07:43 +0200

    make slider properly in background

--- src/screens/BackgroundScreen.tsx
+++ src/screens/BackgroundScreen.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, View, Text } from 'react-native';
 import BackgroundArt from '../../assets/svgs/BackgroundArt';
 import { useBackgroundColors } from '../hooks/useColors';
+import Slider from '@react-native-community/slider';
+import { DateTime } from 'luxon';
 
-export default function BackgroundScreen({...props}) {
-    const animatedColors = useBackgroundColors();
+export default function BackgroundScreen({ ...props }) {
+    const [sliderValue, setSliderValue] = useState<number>(DateTime.now().hour);
+    const animatedColors = useBackgroundColors(sliderValue);
     return (
         <View style={styles.background} {...props}>
+            <Text style={{ margin: 'auto' }}>{animatedColors.value.t}, {sliderValue}</Text>
+            <Slider
+                style={{ width: '70%', height: 40, margin: 'auto' }}
+                minimumValue={0}
+                maximumValue={24}
+                value={sliderValue}
+                onValueChange={(value) => setSliderValue(value)}
+            />
+
             <BackgroundArt size={Dimensions.get("window").width} colors={animatedColors} style={styles.art} />
         </View>
     );
