2de478033397c54106c26fdcef08a23a9c70da25
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2026-02-28 14:22:37 +0200

    change useLocation fallback hierarchy and add timezone lookup

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,4 +1,6 @@
 import * as Location from 'expo-location';
+import cityTimezones from 'city-timezones';
+import { DateTime } from 'luxon';
 import { useStorage } from '../db/storage';
 import { create } from 'zustand';
 
@@ -8,10 +10,12 @@ interface LocationState {
     loading: boolean;
     initialize: () => Promise<void>;
     fetchCurrentLocation: () => Promise<Location.LocationObject | null>;
+    fetchOfflineLocation: () => Promise<Location.LocationObject | null>;
     refresh: () => Promise<void>;
 }
 
-const maxAge = 172800000; // 2*24*60*60*1000 (ms in 2 days)
+const maxAgeLong = 172800000; // 2*24*60*60*1000 (ms in 2 days)
+const maxAgeShort = 7200000; // 2*60*60*1000 (ms in 2 hours)
 
 const useLocation = create<LocationState>((set, get) => ({
     location: null,
@@ -29,39 +33,105 @@ const useLocation = create<LocationState>((set, get) => ({
             return loc;
         } catch (e) {
             set({ location: null, errorMsg: 'Failed to fetch location', loading: false });
+            console.error("[useLocation] Failed to fetch current location:", e);
+            return null;
+        }
+    },
+
+    fetchOfflineLocation: async () => {
+        try {
+            const now = Date.now();
+
+            // check local lastLocation (maxAgeShort)
+            const stored = useStorage.getState().lastLocation;
+            if (stored && now - stored.timestamp <= maxAgeShort) {
+                set({ location: stored, errorMsg: null, loading: false });
+                return stored;
+            }
+
+            // fallback to timezone
+            const zoneName = DateTime.now().zoneName;
+            const cityName = zoneName.split('/').pop()?.replace(/_/g, ' ') || '';
+            const cityLookup = cityTimezones.lookupViaCity(cityName);
+            if (cityLookup && cityLookup.length > 0) {
+                const { lat, lng } = cityLookup[0];
+                const tzLoc: Location.LocationObject = {
+                    coords: {
+                        latitude: lat,
+                        longitude: lng,
+
+                        accuracy: null,
+                        altitude: null,
+                        altitudeAccuracy: null,
+                        heading: null,
+                        speed: null,
+                    },
+                    timestamp: now,
+                };
+                set({ location: tzLoc, errorMsg: null, loading: false });
+                return tzLoc;
+            }
+
+            // fallback to UTC Offset
+            const offsetLoc: Location.LocationObject = {
+                coords: {
+                    latitude: 0,
+                    longitude: (DateTime.local().offset / 4),
+
+                    accuracy: null,
+                    altitude: null,
+                    altitudeAccuracy: null,
+                    heading: null,
+                    speed: null,
+                },
+                timestamp: now,
+            };
+            set({ location: offsetLoc, errorMsg: null, loading: false });
+            return offsetLoc;
+
+        } catch (e) {
+            set({ location: null, errorMsg: 'Failed to fetch location', loading: false });
+            console.error("[useLocation] Failed to fetch offline location:", e);
             return null;
         }
     },
 
     initialize: async () => {
         try {
+            const now = Date.now();
+
+            // PRE PERMISSION REQUEST
+            await get().fetchOfflineLocation();
+
+            // PERMISSION REQUEST
             let currentStatus = (await Location.getForegroundPermissionsAsync()).status;
             if (currentStatus === 'undetermined') {
                 currentStatus = (await Location.requestForegroundPermissionsAsync()).status;
             }
             if (currentStatus !== 'granted') {
-                set({ location: null, errorMsg: 'Permission denied', loading: false });
+                // set({ location: null, errorMsg: 'Permission denied', loading: false });
+                console.error("[useLocation] Permission denied");
                 return;
-            }
+            }  
 
-            let loc = await Location.getLastKnownPositionAsync({
+            // POST PERMISSION REQUEST
+
+            // await LastKnownPosition (maxAgeLong)
+            const knownLoc = await Location.getLastKnownPositionAsync({
                 requiredAccuracy: Location.Accuracy.Low,
-                maxAge
+                maxAge: maxAgeLong,
             })
-
-            if (!loc) {
-                const stored = useStorage.getState().lastLocation;
-                if (stored && Date.now() - stored.timestamp <= maxAge) loc = stored;
+            console.log('known', knownLoc);
+            if (knownLoc) {
+                set({ location: knownLoc, errorMsg: null, loading: false });
+                useStorage.getState().setLastLocation(knownLoc);
             }
 
-            if (loc) {
-                set({ location: loc, errorMsg: null, loading: false });
-                useStorage.getState().setLastLocation(loc);
-            }
-
-            await get().fetchCurrentLocation();
+            // fallback to CurrentLocation async
+            get().fetchCurrentLocation();
         } catch (e) {
             set({ location: null, errorMsg: 'Failed to fetch location', loading: false });
+            console.error("[useLocation] Failed to fetch location:", e);
         }
     },
 
